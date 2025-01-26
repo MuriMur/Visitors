66274f978e3153273a0d724b3508b09f60ea926e
MuriMur <marian.klecherov65@gmail.com>
2025-01-26 15:07:00 +0200

    sql script changes

---
+++ visitors.sql
@@ -0,0 +1,261 @@
+-- MariaDB dump 10.19  Distrib 10.6.11-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: visitors
+-- ------------------------------------------------------
+-- Server version	10.6.11-MariaDB-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `appointment_`
+--
+
+DROP TABLE IF EXISTS `appointment_`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `appointment_` (
+  `appointment_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `visitor_id` bigint(20) DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `user_id` bigint(20) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`appointment_id`),
+  KEY `fk_visitors_idx` (`visitor_id`),
+  KEY `fk_user_idx` (`user_id`),
+  CONSTRAINT `fk_users` FOREIGN KEY (`user_id`) REFERENCES `user_` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_visitors` FOREIGN KEY (`visitor_id`) REFERENCES `visitors` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appointment_`
+--
+
+LOCK TABLES `appointment_` WRITE;
+/*!40000 ALTER TABLE `appointment_` DISABLE KEYS */;
+INSERT INTO `appointment_` VALUES (1,3,'2024-02-25 00:00:00',1,'Инсомния'),(2,5,'2025-11-15 00:00:00',7,'Болки в главата');
+/*!40000 ALTER TABLE `appointment_` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gender`
+--
+
+DROP TABLE IF EXISTS `gender`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gender` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `gender_name` varchar(50) DEFAULT NULL,
+  `description_` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gender`
+--
+
+LOCK TABLES `gender` WRITE;
+/*!40000 ALTER TABLE `gender` DISABLE KEYS */;
+INSERT INTO `gender` VALUES (1,'male',NULL),(2,'female',NULL),(3,'other',NULL);
+/*!40000 ALTER TABLE `gender` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `new_order`
+--
+
+DROP TABLE IF EXISTS `new_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `new_order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `new_order` bigint(20) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `new_order`
+--
+
+LOCK TABLES `new_order` WRITE;
+/*!40000 ALTER TABLE `new_order` DISABLE KEYS */;
+INSERT INTO `new_order` VALUES (1,4);
+/*!40000 ALTER TABLE `new_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `registry`
+--
+
+DROP TABLE IF EXISTS `registry`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `registry` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `visitor_id` bigint(20) NOT NULL,
+  `status` int(11) NOT NULL,
+  `location` varchar(45) DEFAULT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `appointment_id` bigint(20) NOT NULL,
+  `order_` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_visitor_idx` (`visitor_id`),
+  KEY `fk_registry_user_idx` (`user_id`),
+  KEY `fk_registry_appointment_idx` (`appointment_id`),
+  CONSTRAINT `fk_registry_appointment` FOREIGN KEY (`appointment_id`) REFERENCES `appointment_` (`appointment_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_registry_user` FOREIGN KEY (`user_id`) REFERENCES `user_` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_visitor` FOREIGN KEY (`visitor_id`) REFERENCES `visitors` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registry`
+--
+
+LOCK TABLES `registry` WRITE;
+/*!40000 ALTER TABLE `registry` DISABLE KEYS */;
+INSERT INTO `registry` VALUES (1,3,2,'0',1,'2024-02-25 10:34:16',1,''),(2,3,1,'0',1,'2024-02-25 10:34:16',1,'1'),(3,3,1,'0',1,'2024-02-25 10:34:16',1,'2'),(4,5,2,'0',7,'2025-11-15 00:00:00',2,'3'),(5,5,1,'0',7,'2025-11-15 00:00:00',2,'4');
+/*!40000 ALTER TABLE `registry` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role_`
+--
+
+DROP TABLE IF EXISTS `role_`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role_` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(50) DEFAULT NULL,
+  `description_` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role_`
+--
+
+LOCK TABLES `role_` WRITE;
+/*!40000 ALTER TABLE `role_` DISABLE KEYS */;
+INSERT INTO `role_` VALUES (1,'System Administrator','Configures the system and has the rights to manage users.'),(2,'Monitor','Monitors the list of clients and where they are in the queue.'),(3,'Personnel','They will attend visitors'),(4,'Registrator','Registers the visitors'),(5,'Visitor','Visits a personnel');
+/*!40000 ALTER TABLE `role_` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_`
+--
+
+DROP TABLE IF EXISTS `user_`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `birth_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role_id` bigint(20) DEFAULT NULL,
+  `office_number` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_role_idx` (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_`
+--
+
+LOCK TABLES `user_` WRITE;
+/*!40000 ALTER TABLE `user_` DISABLE KEYS */;
+INSERT INTO `user_` VALUES (1,'mur ','mur','2025-01-26 12:48:39','jerry_mouse@abv.bg','MTIzNA==',4,NULL),(2,'Marian','Klecherov','2025-01-26 12:48:39','marianklecherov@abv.bg','MTIzNA==',1,NULL),(3,'Marian ','Doctor','2025-01-26 12:48:39','marianDoctor@gmail.com','MTIzNA==',3,'101'),(4,'marian','klch','2025-01-26 12:48:39','user@abv.bg','MTIzNA==',3,NULL),(5,'monitor','monitor','2025-01-26 12:48:39','monitor@gmail.com','MTIzNA==',2,NULL),(6,'Мариан','Клечеров','2025-01-26 12:48:39','muri@abv.bg','MTIzNA==',0,NULL),(7,'Ivan','Ivanov','2025-01-26 12:48:39','ivanivanov@abv.bg','MTIzNA==',5,NULL);
+/*!40000 ALTER TABLE `user_` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_role`
+--
+
+DROP TABLE IF EXISTS `user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_visitor_idx` (`user_id`),
+  KEY `fk_role_idx` (`role_id`),
+  CONSTRAINT `fk_role` FOREIGN KEY (`role_id`) REFERENCES `role_` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `user_` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_role`
+--
+
+LOCK TABLES `user_role` WRITE;
+/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+INSERT INTO `user_role` VALUES (2,1,2),(3,3,3),(4,3,4);
+/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `visitors`
+--
+
+DROP TABLE IF EXISTS `visitors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `visitors` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(30) DEFAULT NULL,
+  `middle_name` varchar(30) DEFAULT NULL,
+  `last_name` varchar(30) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(21) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `gender_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_gender_idx` (`gender_id`),
+  CONSTRAINT `fk_gender` FOREIGN KEY (`gender_id`) REFERENCES `gender` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `visitors`
+--
+
+LOCK TABLES `visitors` WRITE;
+/*!40000 ALTER TABLE `visitors` DISABLE KEYS */;
+INSERT INTO `visitors` VALUES (3,'tom','tomas','tomas','password2','jerry',NULL,NULL,2),(5,'Иван','Иванов','Иванов','1234','ivanivanov@abv.bg',NULL,NULL,1);
+/*!40000 ALTER TABLE `visitors` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-01-26 12:58:12
